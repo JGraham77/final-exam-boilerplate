1845fe83af4dab830c5438c236b39a0aad19ef3a
Joshua Graham <joshuareynoldsgraham@gmail.com>
2023-09-14 14:19:41 -0500

    comments and deleting some code

--- src/server/server.ts
+++ src/server/server.ts
@@ -8,6 +8,7 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(routes);
 
+// A 404 SPA catcher
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "../public/index.html")));
 
 const port = process.env.PORT || 3000;
